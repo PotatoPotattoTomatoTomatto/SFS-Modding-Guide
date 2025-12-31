cc767386998c465c61b69f4f48b0780f1094e673
امیر <139617633+kojamori@users.noreply.github.com>
2025-12-31 16:40:09 +1030

    add siteconfig

---
+++ config/site-config.ts
@@ -0,0 +1,27 @@
+const config = {
+  gameVersionStack: ["1.5.10.2"], // the last game version is the newest
+  projectName: "SFS Modding Guide",
+  projectTagline: "A comprehensive guide to modding Spaceflight Simulator.",
+  projectRepo: "https://github.com/kojamori/SFS-Modding-Guide",
+  organisationName: "kojamori",
+  socials: {
+    sfsOfficialDiscord: "https://discord.com/invite/hwfWm2d",
+    sfsReddit: "https://www.reddit.com/r/SpaceflightSimulator/",
+    sfsForums: "https://jmnet.one/sfs/forum/index.php",
+    github: "https://github.com/kojamori/SFS-Modding-Guide",
+    discord: "https://discord.gg/QHEmcehAe9",
+  },
+
+  // Docusaurus site metadata
+  headTags: [
+    {
+      tagName: "meta",
+      attributes: {
+        name: "google-site-verification",
+        content: "I3wDwZMuJdx_OvCHIL6KIW8wf6BwmkOZpx_Cci_Ol1w",
+      },
+    },
+  ],
+};
+
+export default config;
